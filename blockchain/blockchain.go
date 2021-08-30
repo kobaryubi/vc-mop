1b2cb1fb7258d617cb13cbec4e5cbc688ce70174
MasahikoKobayashi <kobaryubi6@gmail.com>
2021-08-30 23:53:36 +0900

    linter追加など

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -4,12 +4,14 @@ import (
 	"time"
 )
 
+// Block ブロックチェーンのブロックを定義する型
 type Block struct {
 	prevHash     [32]byte
 	transactions []string
 	timestamp    int64
 }
 
+// NewBlock ブロックを作成する関数
 func NewBlock(prevHash [32]byte, transactions []string) *Block {
 	b := &Block{}
 
